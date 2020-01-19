21a732e8e0c0e339fb64ba1fcca40db8be7c3996
Seunghoon Jang <spangiga@gmail.com>
2020-01-20 01:13:09 +0900

    20.1.20

---
+++ DBMS/app/src/main/java/RealmReadActivity.java
@@ -0,0 +1,32 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dbms.R;
+
+import io.realm.Realm;
+
+public class RealmReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realm_read);
+
+        TextView titleView=findViewById(R.id.realm_read_title);
+        TextView contentView=findViewById(R.id.realm_read_content);
+
+        Intent intent=getIntent();
+        String title=intent.getStringExtra("title");
+
+        Realm mRealm=Realm.getDefaultInstance();
+
+        MemoVO vo = mRealm.where(MemoVO.class).equalTo("title", title).findFirst();
+
+        titleView.setText(vo.title);
+        contentView.setText(vo.content);
+
+    }
+}
