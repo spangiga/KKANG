46e9b673b6b6ee0bc9e6a0e15abe01cd67ae50a2
miniesam <46525418+miniesam@users.noreply.github.com>
2020-01-21 02:00:57 +0900

    Create part3_8

---
+++ miniesam/part3_8/src/main/java/com/example/part3_8/RealmReadActivity.java
@@ -0,0 +1,30 @@
+package com.example.part3_8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.realm.Realm;
+
+public class RealmReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realm_read);
+
+        TextView titleView = findViewById(R.id.realm_read_title);
+        TextView contentView = findViewById(R.id.realm_read_content);
+
+        // 넘어온 title 문자열 추출
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+
+        Realm mRealm = Realm.getDefaultInstance();
+        MemoVO vo = mRealm.where(MemoVO.class).equalTo("title", title).findFirst();
+        titleView.setText(vo.title);
+        contentView.setText(vo.content);
+    }
+}
